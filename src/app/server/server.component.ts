aad51b0a606d08582e9e0b5f610cb131f751259d
Aymediacoán Mauleón <yaconeill.amq@gmail.com>
2019-05-07 19:43:07 +0200

    update server controller

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,11 +8,29 @@ import { Server } from '../shared/server';
 })
 export class ServerComponent implements OnInit {
 
+  color: string;
+  buttonText: string;
   @Input() server: Server;
 
   constructor() { }
 
   ngOnInit() {
+    this.setServerAction(this.server.isOnline);
   }
 
+  setServerAction(isOnline: boolean) {
+    if (isOnline) {
+      this.server.isOnline = true;
+      this.color = '#66BB6A'
+      this.buttonText = 'Shut Down'
+    } else{
+      this.server.isOnline = false;
+      this.color = '#FF6B6B'
+      this.buttonText = 'Start'
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    this.setServerAction(!onlineStatus);
+  }
 }
